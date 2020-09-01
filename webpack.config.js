ea89a9b55c8b10ad40e894b678a20471a505b05c
finus16 <m_zaba@wp.pl>
2020-09-01 22:27:16 +0200

    css rules

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,18 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+
+    module: {
+
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    }
 };
