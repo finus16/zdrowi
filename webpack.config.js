5344e6ec404be9fd6fef896898d477db05e4c9b7
finus16 <m_zaba@wp.pl>
2020-09-07 21:41:25 +0200

    Fileloader

--- webpack.config.js
+++ webpack.config.js
@@ -13,16 +13,23 @@ module.exports = {
         new MiniCssExtractPlugin({ filename: "[name].[contentHash].css" }),
         new HtmlWebackPlugin({ template: "./src/index.html"})
     ],
+
     module: {
 
         rules: [
+
             {
-                test: /\.s[ac]ss$/,
+                test: /\.css|s[ac]ss$/,
                 use: [
                     MiniCssExtractPlugin.loader, //3. Extract css into files
                     'css-loader',
                     'sass-loader'
                 ]
+            },
+
+            {
+                test: /\.(ttf|eot|svg|woff(2)?)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: "file-loader"
             }
         ]
     }
