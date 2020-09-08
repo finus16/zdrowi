11d57bef2ba26d990577042f82523f900ad6ed48
finus16 <m_zaba@wp.pl>
2020-09-08 23:07:18 +0200

    Scrolls on link click

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import _ from 'lodash';
 import './style.scss';
 import 'bootstrap';
+
+import $ from 'jquery';
+
+$('.navbar-nav a').on('click', function(e){
+    let url = $(this).attr('href');
+    let pos = url.indexOf("#");
+    let anchor = pos != -1 ? url.substring(pos + 1) : null;
+    if(anchor){
+        let target = $('#' + anchor);
+        if(target.length != 0){
+            e.preventDefault();
+            $('.navbar-collapse').collapse('toggle');
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 800);
+        }
+    }
+});
