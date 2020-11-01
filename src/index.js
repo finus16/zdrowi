25d26bcab6d6284d141926cdab2977201ea76fc3
finus16 <m_zaba@wp.pl>
2020-11-01 18:28:04 +0100

    Page fixes

--- src/index.js
+++ src/index.js
@@ -15,3 +15,18 @@ $('.navbar-nav a').on('click', function(e){
         }
     }
 });
+
+$('a .btn').on('click', function(e){
+    let url = $(this).attr('href');
+    let pos = url.indexOf("#");
+    let anchor = pos != -1 ? url.substring(pos + 1) : null;
+    if(anchor){
+        let target = $('#' + anchor);
+        if(target.length != 0){
+            e.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 800);
+        }
+    }
+});
