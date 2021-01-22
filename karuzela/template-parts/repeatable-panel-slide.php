073ddfadf4dbfc58c9cec343b2f161df70628a42
finus16 <m_zaba@wp.pl>
2021-01-22 15:49:11 +0100

    Removed seo tab from plugin

--- karuzela/template-parts/repeatable-panel-slide.php
+++ karuzela/template-parts/repeatable-panel-slide.php
@@ -15,21 +15,12 @@
     <div class="otb-tabs-container">
         <ul class="tabs">
             <li><a data-tab="content" class="active"><?php esc_html_e( 'Content', 'super-simple-slider' ); ?></a></li>
-            <li><a data-tab="seo">SEO</a></li>
         </ul>
 
         <!-- Content -->
         <div class="tab-content content active">
             <?php $this->create_justart_form_control( 'justart_slider_slide_title', $this->repeatable_fieldset_settings ); ?>
             <?php $this->create_justart_form_control( 'justart_slider_slide_description', $this->repeatable_fieldset_settings ); ?>
-        </div>
-        
-        <!-- SEO -->
-        <div class="tab-content seo">
-            <?php 
-                $this->create_justart_form_control( 'super_simple_slider_slide_image_alt', $this->repeatable_fieldset_settings );
-                $this->create_justart_form_control( 'super_simple_slider_slide_image_title', $this->repeatable_fieldset_settings ); 
-            ?>
         </div>
         
     </div>
